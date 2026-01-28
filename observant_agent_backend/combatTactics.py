fe0f43f5da6d352d6570bcc861ca779512593a2c
paulkent-coder <paulkent-041@gmail.com>
2026-01-27 21:41:13 -0500

    Cleaned up formatting for combatTactics.py and doctor.py

--- observant_agent_backend/combatTactics.py
+++ observant_agent_backend/combatTactics.py
@@ -1,45 +1,49 @@
 #!/usr/bin/python
 
 def isWorthFighting(state, observations, monster):
-	hostility = monster.hostility
-	glyph = monster.glyph
-	
-	# TODO: Add more logic! :D
-	
-	if hostility != 1:
-		# Maybe later we'll consider being mean to peacefuls
-		# But in any case we definitely don't want to be mean to pets
-		return False
-	
-	if glyph == 27 and observations["blstats"][10] < 21:
-		# Oh boy, a gas spore. Yikes.
-		# At 21 HP, a gas spore explosion has a 2.7% chance to kill us, which I'm willing to put up with.
-		# If our HP is any lower than that, let's not take the risk.
-		return False
-	
-	if glyph == 28:
-		# Hitting a floating eye is asking for trouble.
-		# TODO: Check if we're blind, because if we're blind we're fine to hit it.
-		return False
-	
-	if (glyph >= 156 and glyph <= 159) or glyph == 55 or glyph == 56:
-		# The molds are sessile and have painful passive counterattacks, so don't antagonize them
-		return False
-	
-	return True
+    hostility = monster.hostility
+    glyph = monster.glyph
+
+    # TODO: Add more logic! :D
+
+    if hostility != 1:
+        # Maybe later we'll consider being mean to peacefuls
+        # But in any case we definitely don't want to be mean to pets
+        return False
+
+    if glyph == 27 and observations["blstats"][10] < 21:
+        # Oh boy, a gas spore. Yikes.
+        # At 21 HP, a gas spore explosion has a 2.7% chance to kill us,
+        # which I'm willing to put up with.
+        # If our HP is any lower than that, let's not take the risk.
+        return False
+
+    if glyph == 28:
+        # Hitting a floating eye is asking for trouble.
+        # TODO: Check if we're blind, because if we're blind
+        # we're fine to hit it.
+        return False
+
+    if (glyph >= 156 and glyph <= 159) or glyph == 55 or glyph == 56:
+        # The molds are sessile and have painful passive counterattacks,
+        # so don't antagonize them
+        return False
+
+    return True
+
 
 def meleeCombat(state, observations):
-	from .utilities import iterableOverVicinity
-	dirs = iterableOverVicinity(observations)
-	for direction in range(len(dirs)):
-		if dirs[direction] == None:
-			# out of bounds
-			continue
-		row, col = dirs[direction]
-		monster = state.get("tracker").tattle(row,col)
-		if monster == None:
-			continue
-		if isWorthFighting(state, observations, monster):
-			state.get("queue").append(direction)
-			return 39 # fight
-	return -1
+    from .utilities import iterableOverVicinity
+    dirs = iterableOverVicinity(observations)
+    for direction in range(len(dirs)):
+        if dirs[direction] == None:
+            # out of bounds
+            continue
+        row, col = dirs[direction]
+        monster = state.get("tracker").tattle(row, col)
+        if monster == None:
+            continue
+        if isWorthFighting(state, observations, monster):
+            state.get("queue").append(direction)
+            return 39  # fight
+    return -1
--- observant_agent_backend/doctor.py
+++ observant_agent_backend/doctor.py
@@ -4,143 +4,169 @@ from .gamestate import StateModule
 from .agent_config import *
 from .utilities import *
 
-# "Permafood" = food that doesn't have an expiration date (and that we can't really use for any other purpose)
-permafood = [1299, 2148, 2149, 2150, 2151, 2158, 2159, 2160, 2161, 2162, 2163, 2166, 2168, 2170, 2172, 2173, 2174, 2175, 2176] # TODO: Add 2177 (tin)
+# "Permafood" = food that doesn't have an expiration date
+# (and that we can't really use for any other purpose)
+permafood = [1299, 2148, 2149, 2150, 2151, 2158, 2159,
+             2160, 2161, 2162, 2163, 2166, 2168, 2170,
+             2172, 2173, 2174, 2175, 2176]  # TODO: Add 2177 (tin)
+
 
 class StatusChecker(StateModule):
-	def __init__(self, state):
-		self.currentAilments = []
-		self.lastKnownStatus = 0
-		self.lastKnownHunger = 0
-		self.lastKnownEncumbrance = 0
-		self.state = state
-		self.printedSituation = False
-		
-		self.stats_runs = 0
-		self.stats_starves = 0
-	def reset(self): 
-		if not CONST_QUIET:
-			status = self.reportStatus()
-			if not len(status) == 0:
-				print("Status at death: ",self.reportStatus())
-		
-		self.stats_runs += 1
-		if self.lastKnownHunger >= 4:
-			self.stats_starves += 1
-		
-		self.currentAilments = []
-		self.lastKnownStatus = 0
-		self.lastKnownHunger = 0
-		self.lastKnownEncumbrance = 0
-		self.printedSituation = False
-	def dumpCore(self):
-		status = self.reportStatus()
-		if not len(status) == 0:
-			print("Status at death: ",self.reportStatus())			
-	def displayStats(self):
-		print("Proportion of runs ending in starvation:",(self.stats_starves / self.stats_runs))
+    def __init__(self, state):
+        self.currentAilments = []
+        self.lastKnownStatus = 0
+        self.lastKnownHunger = 0
+        self.lastKnownEncumbrance = 0
+        self.state = state
+        self.printedSituation = False
 
-	def recordStatus(self, observations):
-		self.lastKnownStatus = observations["blstats"][nethack.NLE_BL_CONDITION]
-		self.lastKnownHunger = observations["blstats"][nethack.NLE_BL_HUNGER]
-		self.lastKnownEncumbrance = observations["blstats"][nethack.NLE_BL_CAP]
-		
-		if (not self.printedSituation) and self.lastKnownHunger >= 4:
-			self.printedSituation = True
-			printScreen(observations)
-		
-		return -1
-	def logAilment(self, ailment):
-		if(not ailment in self.currentAilments):
-			self.currentAilments.append(ailment)
-	def checkAilment(self, ailment):
-		return ailment in self.currentAilments
-	def cureAilment(self, ailment):
-		self.currentAilments.remove(ailment)
-	def reportStatus(self):
-		result = self.currentAilments.copy()
-		if (self.lastKnownStatus & nethack.BL_MASK_STONE):
-			result.append("Stoning")
-		if (self.lastKnownStatus & nethack.BL_MASK_SLIME):
-			result.append("Sliming")
-		if (self.lastKnownStatus & nethack.BL_MASK_STRNGL):
-			result.append("Strangled")
-		if (self.lastKnownStatus & nethack.BL_MASK_FOODPOIS):
-			result.append("Food Poisoning")
-		if (self.lastKnownStatus & nethack.BL_MASK_TERMILL):
-			result.append("Illness")
-		if (self.lastKnownStatus & nethack.BL_MASK_BLIND):
-			result.append("Blindness")
-		if (self.lastKnownStatus & nethack.BL_MASK_DEAF):
-			result.append("Deafness")
-		if (self.lastKnownStatus & nethack.BL_MASK_STUN):
-			result.append("Stunning")
-		if (self.lastKnownStatus & nethack.BL_MASK_CONF):
-			result.append("Confusion")
-		if (self.lastKnownStatus & nethack.BL_MASK_HALLU):
-			result.append("Hallucination")
-		if (self.lastKnownStatus & nethack.BL_MASK_LEV):
-			result.append("Levitation")
-		if (self.lastKnownStatus & nethack.BL_MASK_FLY):
-			result.append("Flight")
-		if self.lastKnownHunger == 0:
-			result.append("Satiation")
-		if self.lastKnownHunger == 2:
-			result.append("Hunger")
-		if self.lastKnownHunger == 3:
-			result.append("Severe Hunger")
-		if self.lastKnownHunger >= 4:
-			result.append("Starvation")
-		if self.lastKnownEncumbrance == 1:
-			result.append("Burden")
-		if self.lastKnownEncumbrance == 2:
-			result.append("Stress")
-		if self.lastKnownEncumbrance == 3:
-			result.append("Strain")
-		if self.lastKnownEncumbrance == 4:
-			result.append("Overtaxing")
-		if self.lastKnownEncumbrance >= 5:
-			result.append("Overloading")
-		return result
-	def checkMajorStatus(self, status):
-		return self.lastKnownStatus & status
-	def fixUrgentProblems(self, observations):
-		# An urgent problem is defined as one which can't wait a few turns
-		# for you to kill a monster first. It might be something that
-		# inhibits your ability to fight, or something that will probably
-		# kill you by the time you're done.
-		
-		# Other problems go in fixMinorProblems above.
-		hp, max = readHeroHealth(observations)
-		if(hp <= 4 or max / hp >= 4):
-			desired = ["potion of healing","potion of extra healing","potion of full healing"]
-			desired += ["potions of healing","potions of extra healing","potions of full healing"]
-			letters, types, indices = self.state.get("inventory").reachForString(observations, desired)
-			if(len(letters) > 0):
-				self.state.get("queue").append(False)
-				self.state.get("queue").append(parse([letters[0]]))
-				return 64 # quaff
-		if self.lastKnownHunger >= 3 and self.lastKnownEncumbrance < 4:
-			letters, types, indices = self.state.get("inventory").reachForItem(observations, permafood)
-			if(len(letters) > 0):
-				self.state.get("queue").append(False)
-				self.state.get("queue").append(parse([letters[0]]))
-				return 35 # eat
-		return -1
-	def fixMinorProblems(self, observations):
-		if self.lastKnownHunger >= 2 and self.lastKnownEncumbrance < 4:
-			letters, types, indices = self.state.get("inventory").reachForItem(observations, permafood)
-			if(len(letters) > 0):
-				self.state.get("queue").append(False)
-				self.state.get("queue").append(parse([letters[0]]))
-				return 35 # eat
-		return -1
+        self.stats_runs = 0
+        self.stats_starves = 0
+
+    def reset(self):
+        if not CONST_QUIET:
+            status = self.reportStatus()
+            if not len(status) == 0:
+                print("Status at death: ", self.reportStatus())
+
+        self.stats_runs += 1
+        if self.lastKnownHunger >= 4:
+            self.stats_starves += 1
+
+        self.currentAilments = []
+        self.lastKnownStatus = 0
+        self.lastKnownHunger = 0
+        self.lastKnownEncumbrance = 0
+        self.printedSituation = False
+
+    def dumpCore(self):
+        status = self.reportStatus()
+        if not len(status) == 0:
+            print("Status at death: ", self.reportStatus())
+
+    def displayStats(self):
+        print("Proportion of runs ending in starvation:",
+              (self.stats_starves / self.stats_runs))
+
+    def recordStatus(self, observations):
+        blstats = observations["blstats"]
+        self.lastKnownStatus = blstats[nethack.NLE_BL_CONDITION]
+        self.lastKnownHunger = blstats[nethack.NLE_BL_HUNGER]
+        self.lastKnownEncumbrance = blstats[nethack.NLE_BL_CAP]
+
+        if (not self.printedSituation) and self.lastKnownHunger >= 4:
+            self.printedSituation = True
+            printScreen(observations)
+
+        return -1
+
+    def logAilment(self, ailment):
+        if (ailment not in self.currentAilments):
+            self.currentAilments.append(ailment)
+
+    def checkAilment(self, ailment):
+        return ailment in self.currentAilments
+
+    def cureAilment(self, ailment):
+        self.currentAilments.remove(ailment)
+
+    def reportStatus(self):
+        result = self.currentAilments.copy()
+        if (self.lastKnownStatus & nethack.BL_MASK_STONE):
+            result.append("Stoning")
+        if (self.lastKnownStatus & nethack.BL_MASK_SLIME):
+            result.append("Sliming")
+        if (self.lastKnownStatus & nethack.BL_MASK_STRNGL):
+            result.append("Strangled")
+        if (self.lastKnownStatus & nethack.BL_MASK_FOODPOIS):
+            result.append("Food Poisoning")
+        if (self.lastKnownStatus & nethack.BL_MASK_TERMILL):
+            result.append("Illness")
+        if (self.lastKnownStatus & nethack.BL_MASK_BLIND):
+            result.append("Blindness")
+        if (self.lastKnownStatus & nethack.BL_MASK_DEAF):
+            result.append("Deafness")
+        if (self.lastKnownStatus & nethack.BL_MASK_STUN):
+            result.append("Stunning")
+        if (self.lastKnownStatus & nethack.BL_MASK_CONF):
+            result.append("Confusion")
+        if (self.lastKnownStatus & nethack.BL_MASK_HALLU):
+            result.append("Hallucination")
+        if (self.lastKnownStatus & nethack.BL_MASK_LEV):
+            result.append("Levitation")
+        if (self.lastKnownStatus & nethack.BL_MASK_FLY):
+            result.append("Flight")
+        if self.lastKnownHunger == 0:
+            result.append("Satiation")
+        if self.lastKnownHunger == 2:
+            result.append("Hunger")
+        if self.lastKnownHunger == 3:
+            result.append("Severe Hunger")
+        if self.lastKnownHunger >= 4:
+            result.append("Starvation")
+        if self.lastKnownEncumbrance == 1:
+            result.append("Burden")
+        if self.lastKnownEncumbrance == 2:
+            result.append("Stress")
+        if self.lastKnownEncumbrance == 3:
+            result.append("Strain")
+        if self.lastKnownEncumbrance == 4:
+            result.append("Overtaxing")
+        if self.lastKnownEncumbrance >= 5:
+            result.append("Overloading")
+        return result
+
+    def checkMajorStatus(self, status):
+        return self.lastKnownStatus & status
+
+    def fixUrgentProblems(self, observations):
+        # An urgent problem is defined as one which can't wait a few turns
+        # for you to kill a monster first. It might be something that
+        # inhibits your ability to fight, or something that will probably
+        # kill you by the time you're done.
+
+        # Other problems go in fixMinorProblems above.
+        inventory = self.state.get("inventory")
+        queue = self.state.get("queue")
+        hp, max = readHeroHealth(observations)
+        if (hp <= 4 or max / hp >= 4):
+            desired = ["potion of healing",
+                       "potion of extra healing",
+                       "potion of full healing",
+                       "potions of healing",
+                       "potions of extra healing",
+                       "potions of full healing"]
+            letters, _, _ = inventory.reachForString(observations, desired)
+            if (len(letters) > 0):
+                queue.append(False)
+                queue.append(parse([letters[0]]))
+                return 64  # quaff
+        if self.lastKnownHunger >= 3 and self.lastKnownEncumbrance < 4:
+            letters, _, _ = inventory.reachForItem(observations, permafood)
+            if (len(letters) > 0):
+                queue.append(False)
+                queue.append(parse([letters[0]]))
+                return 35  # eat
+        return -1
+
+    def fixMinorProblems(self, observations):
+        inventory = self.state.get("inventory")
+        if self.lastKnownHunger >= 2 and self.lastKnownEncumbrance < 4:
+            letters, _, _ = inventory.reachForItem(observations, permafood)
+            if (len(letters) > 0):
+                self.state.get("queue").append(False)
+                self.state.get("queue").append(parse([letters[0]]))
+                return 35  # eat
+        return -1
+
 
 def checkup(state, observations):
-	return state.get("doctor").recordStatus(observations)
+    return state.get("doctor").recordStatus(observations)
+
 
 def fixMinorProblems(state, observations):
-	return state.get("doctor").fixMinorProblems(observations)
+    return state.get("doctor").fixMinorProblems(observations)
+
 
 def fixUrgentProblems(state, observations):
-	return state.get("doctor").fixUrgentProblems(observations)
+    return state.get("doctor").fixUrgentProblems(observations)
